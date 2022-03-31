af68f0ef0b3914bc004138343f09be4d83e9f609
brandon_w laptop <bwoo14@my.bcit.ca>
2022-03-31 09:58:24 -0700

    add functionality to clear the copies button
    the clear the copies button now works by query selecting all of the child divs in the copy container element and then using a for loop to delete them all with the .remove() function

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
+// Clone 
 const copyContainer = document.querySelector('.copy-container')
 const addDivClick = document.querySelector('.original-container')
 
-
-
-// USE VARIABLES INSTEAD OF JUST ONE
-
-
 function addCopy(evt) {
     const copyTemplate = `
-    <div>
-        <p>${evt.target.innerHTML}<p>           
+    <div class='copy'>
+        <p>${evt.target.innerHTML}</p>           
     </div>
     `
-    console.log(evt.target.innerHTML)
     copyContainer.insertAdjacentHTML("afterbegin", copyTemplate)
 }
 
 addDivClick.addEventListener('click', addCopy)
+
+
+// Delete
+
+const clearButton = document.querySelector('button')
+
+
+function clearCopies(evt) {
+    console.log(evt)
+    const copies = document.querySelectorAll('.copy')
+    console.log(copies)
+    
+    for (copy of copies) {
+        copy.remove()
+    }
+}
+
+clearButton.addEventListener('click', clearCopies)
